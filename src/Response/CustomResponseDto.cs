08a7f3b7aec253dbd08261f5bbd5d7432182ea0b
Elvys Marcos Ribeiro Bispo <elvys.marcos@gmail.com>
2024-09-23 19:21:46 -0300

    fix: Fixs format

--- src/Response/CustomResponseDto.cs
+++ src/Response/CustomResponseDto.cs
@@ -1,4 +1,8 @@
 namespace IATec.Shared.Api.Response;
 
-public sealed record CustomResponseDto(bool Success, int StatusCode, object? Data, List<string> Messages, 
+public sealed record CustomResponseDto(
+    bool Success,
+    int StatusCode,
+    object? Data,
+    List<string> Messages,
     DateTimeOffset DateTimeUtc);
